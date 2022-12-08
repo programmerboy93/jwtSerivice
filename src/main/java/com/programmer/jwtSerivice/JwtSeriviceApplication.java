2db6fffd47c166a30267ab21b429d7d88378e5e4
lukasz <lukaszsikorski19933@gmail.com>
2022-12-08 13:55:51 +0100

    Initial commit

---
+++ src/main/java/com/programmer/jwtSerivice/JwtSeriviceApplication.java
@@ -0,0 +1,13 @@
+package com.programmer.jwtSerivice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSeriviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSeriviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programmer/jwtSerivice/JwtSeriviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmer.jwtSerivice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSeriviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
